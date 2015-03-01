7bce29c7dc9b8abdeda0e956edfd8d0deccddc5c
Leonid Shagabutdinov <leonid@shagabutidnov.com>
2015-03-01 12:42:53 +0600

    harmful files removed (issue #2)

--- OpenFilePath.py
+++
@@ -1,9 +0,0 @@
-from OpenPath import open_path
-import sublime_plugin, os
-
-class OpenFileFolder( sublime_plugin.WindowCommand ):
-  def run( self ):
-    if self.window.active_view() is None:
-      return
-
-    open_path.open( os.path.dirname( self.window.active_view().file_name() ) )
--- OpenProjectPath.py
+++
@@ -1,6 +0,0 @@
-from OpenPath import open_path
-import sublime_plugin
-
-class OpenProjectFolder( sublime_plugin.WindowCommand ):
-  def run( self ):
-    open_path.open( self.window.folders()[0] )
